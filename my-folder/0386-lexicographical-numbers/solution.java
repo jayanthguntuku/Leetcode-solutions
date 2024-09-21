b78123663d8eadff3f44b87c405154f0d2c818c4
jayanthguntuku <guntukujayanth999@gmail.com>
2024-09-21 02:45:46 +0000

    [LeetCode Sync] Runtime - 5 ms (63.63%), Memory - 48.6 MB (98.68%)

---
+++ my-folder/0386-lexicographical-numbers/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        int var = 1;
+        int temp = 2;
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i =1; i<=n; i++){
+            list.add(var);
+            if(var*10<=n){
+                var = var * 10;
+            }
+            else{
+                while(var % 10 == 9 || var >= n){
+                    var = var/10;
+                }
+                var+=1;
+            }
+        }
+        return list;
+    }
+}
