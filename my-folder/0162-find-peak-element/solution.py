85b5203c77c447199b02152941b574e23e9e205e
jayanthguntuku <guntukujayanth999@gmail.com>
2026-08-04 06:03:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (79.41%)

---
+++ my-folder/0162-find-peak-element/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        start = 0
+        end = n-1
+
+        while(start < end):
+            mid = start + (end - start)//2
+
+            if nums[mid] < nums[mid+1]:
+                start = mid + 1
+            else:
+                end = mid
+        return start
+ 
