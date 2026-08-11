a3b3abbaf0f5752a79a05a32d08918bfe4a73b56
jayanthguntuku <guntukujayanth999@gmail.com>
2026-08-11 01:06:46 +0000

    [LeetCode Sync] Runtime - 38 ms (49.93%), Memory - 31.3 MB (94.24%)

---
+++ my-folder/0053-maximum-subarray/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        curr_sum = nums[0]
+        max_sum = nums[0]
+        
+        for i in range(1,n):
+            curr_sum = max(curr_sum + nums[i], nums[i])
+            max_sum = max(max_sum, curr_sum)
+        return max_sum
+
