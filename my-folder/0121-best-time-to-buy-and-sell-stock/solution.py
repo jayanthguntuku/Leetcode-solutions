02ed343e0bc12d6809f96ab766d9bdcf5a612bdd
jayanthguntuku <guntukujayanth999@gmail.com>
2026-08-12 04:15:12 +0000

    [LeetCode Sync] Runtime - 47 ms (58.88%), Memory - 29.1 MB (7.27%)

---
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        best_profit = 0
+        for price in prices[1:]:
+            best_profit = max(best_profit, price - min_price)
+            min_price = min(min_price, price)
+        return best_profit
